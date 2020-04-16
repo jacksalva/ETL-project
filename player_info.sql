d9c355084d45aab1c0a9f08e8ef6744bc27560df
Jack Salva <johnmsalva@gmail.com>
2020-04-15 20:47:03 -0500

    added sql files

---
+++ player_info.sql
@@ -0,0 +1,6 @@
+CREATE TABLE player_info (
+id TEXT PRIMARY KEY,
+Name TEXT,
+Position TEXT,
+Overall int
+);
---
+++ pro_football_reference.sql
@@ -0,0 +1,16 @@
+CREATE TABLE pro_football_reference (
+	id TEXT,
+	age INT,
+	position TEXT,
+	birth_place TEXT,
+	college TEXT,
+	current_status TEXT,
+	current_team TEXT,
+	experience VARCHAR,
+	height INT,
+	high_school TEXT,
+	name TEXT,
+	number INT,
+	weight_lbs INT,
+	years_played VARCHAR
+);
